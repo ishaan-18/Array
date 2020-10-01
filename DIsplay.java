29cd92cf2c37710d991530f04d25db15f0787709
ishaan-18 <ishaan-18>
2020-10-01 22:01:02 +0530

    Arrays Problems

---
+++ DIsplay.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Display {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ SquareArray.java
@@ -0,0 +1,8 @@
+public class SquareArray {
+    public static void main(String[] args) {
+        int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+        for (int val : arr1) {
+            System.out.println(val * val);
+        }
+    }
+}
